4cfbca9c2645de8eb15bb4993d7bce97aa49f21a
Bastian Rinsche <bastian.rinsche@gmail.com>
2016-05-15 22:38:55 +0200

    Implement color & orientation change handling

--- app/src/main/java/com/panic/sign/SignActivity.java
+++ app/src/main/java/com/panic/sign/SignActivity.java
@@ -1,7 +1,13 @@
 package com.panic.sign;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,20 +15,55 @@ import android.widget.ImageView;
 
 import java.util.List;
 
+import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 
 public class SignActivity extends AppCompatActivity {
+    static {
+        // https://medium.com/@chrisbanes/appcompat-v23-2-age-of-the-vectors-91cbafa87c88
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
     @BindView(R.id.button_change) Button change;
+    @BindView(R.id.view_sign) ImageView signView;
     @BindView(R.id.control_top) View topColors;
     @BindView(R.id.control_bottom) View bottomColors;
+    @BindDrawable(R.drawable.sign_all) Drawable rawSign;
     // this is necessary to disambiguate the layout IDs
     // may be fixed in the future https://github.com/JakeWharton/butterknife/issues/394
     static class ColorControl {
         @BindViews({R.id.red, R.id.orange, R.id.yellow, R.id.green, R.id.green2,
                 R.id.teal, R.id.light_blue, R.id.blue, R.id.purple, R.id.pink})
         List<ImageView> colors;
+
+        private void deselectViews() {
+            for (View v : colors) {
+                v.setSelected(false);
+            }
+        }
+
+        public String getSelected() {
+            for (View v : colors) {
+                if (v.isSelected()) {
+                    return v.getTag().toString();
+                }
+            }
+            return null;
+        }
+
+        public void setSelected(String selection) {
+            if (selection == null) {
+                return;
+            }
+            deselectViews();
+            for (View v : colors) {
+                if (v.getTag().toString().equals(selection)) {
+                    v.setSelected(true);
+                    break;
+                }
+            }
+        }
     }
 
     ColorControl topControl = new ColorControl();
@@ -30,8 +71,12 @@ public class SignActivity extends AppCompatActivity {
 
     boolean top = true;
     boolean bottom = false;
-    String topSelection = "NONE";
-    String bottomSelection = "NONE";
+    String topSelection = "LIGHTBLUE";
+    String bottomSelection = "BLUE";
+
+    LayerDrawable sign;
+    Drawable topSign;
+    Drawable bottomSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +87,21 @@ public class SignActivity extends AppCompatActivity {
         ButterKnife.bind(topControl, topColors);
         ButterKnife.bind(bottomControl, bottomColors);
 
+        sign = (LayerDrawable)rawSign;
+        topSign = sign.findDrawableByLayerId(R.id.sign_top);
+        bottomSign = sign.findDrawableByLayerId(R.id.sign_bottom);
+
+        signView.setImageDrawable(sign);
+
+        if (savedInstanceState != null) {
+            topSelection = savedInstanceState.getString("TOP", "NONE");
+            bottomSelection = savedInstanceState.getString("BOTTOM", "NONE");
+        }
+        topControl.setSelected(topSelection);
+        changeSignColor(top, topSelection);
+        bottomControl.setSelected(bottomSelection);
+        changeSignColor(bottom, bottomSelection);
+
         View.OnClickListener topListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,9 +110,10 @@ public class SignActivity extends AppCompatActivity {
                     view.setSelected(false);
                     topSelection = "NONE";
                 } else {
-                    deselectViews(top);
+                    topControl.deselectViews();
                     view.setSelected(true);
                     topSelection = current;
+                    changeSignColor(top, current);
                 }
                 Log.d("topSelection", topSelection);
             }
@@ -66,9 +127,10 @@ public class SignActivity extends AppCompatActivity {
                     view.setSelected(false);
                     bottomSelection = "NONE";
                 } else {
-                    deselectViews(bottom);
+                    bottomControl.deselectViews();
                     view.setSelected(true);
                     bottomSelection = current;
+                    changeSignColor(bottom, current);
                 }
                 Log.d("bottomSelection", bottomSelection);
             }
@@ -81,19 +143,70 @@ public class SignActivity extends AppCompatActivity {
         for (ImageView i : bottomControl.colors) {
             i.setOnClickListener(bottomListener);
         }
-
     }
 
-    private void deselectViews(boolean top) {
-        if (top) {
-            for (View v : topControl.colors) {
-                v.setSelected(false);
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("TOP", topControl.getSelected());
+        outState.putString("BOTTOM", bottomControl.getSelected());
+    }
+
+
+    private void changeSignColor(boolean top, String colorString) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+            if (top) {
+                topSign.setColorFilter(resolveColor(colorString), PorterDuff.Mode.MULTIPLY);
+            } else {
+                bottomSign.setColorFilter(resolveColor(colorString), PorterDuff.Mode.MULTIPLY);
             }
         } else {
-            for (View v : bottomControl.colors) {
-                v.setSelected(false);
+            if (top) {
+                topSign.setTint(resolveColor(colorString));
+            } else {
+                bottomSign.setTint(resolveColor(colorString));
             }
         }
+    }
+
+    private int resolveColor(String color) {
+        int id;
+        switch (color) {
+            case "RED":
+                id = R.color.red;
+                break;
+            case "ORANGE":
+                id = R.color.orange;
+                break;
+            case "YELLOW":
+                id = R.color.yellow;
+                break;
+            case "GREEN":
+                id = R.color.green;
+                break;
+            case "GREEN2":
+                id = R.color.green2;
+                break;
+            case "TEAL":
+                id = R.color.teal;
+                break;
+            case "LIGHTBLUE":
+                id = R.color.lightblue;
+                break;
+            case "BLUE":
+                id = R.color.blue;
+                break;
+            case "PURPLE":
+                id = R.color.purple;
+                break;
+            case "PINK":
+                id = R.color.pink;
+                break;
+            default:
+                id = R.color.purple;
+                break;
+        }
+        return ContextCompat.getColor(this, id);
     }
 
 }
