91cfe128f0316465f473e562d421751b4d040902
Bastian Rinsche <bastian.rinsche@gmail.com>
2016-05-17 14:59:03 +0200

    Refactor xml color keys

--- app/src/main/java/com/panic/sign/ColorControl.java
+++ app/src/main/java/com/panic/sign/ColorControl.java
@@ -17,7 +17,7 @@ public class ColorControl {
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            String current = view.getTag().toString();
+            String current = (String)view.getTag();
             if (!selection.equals(current)) {
                 selection = current;
                 setSelected(selection);
@@ -39,7 +39,7 @@ public class ColorControl {
     public String getSelected() {
         for (View v : colors) {
             if (v.isSelected()) {
-                return v.getTag().toString();
+                return (String)v.getTag();
             }
         }
         throw new IllegalStateException("One color has to be selected at all times");
--- app/src/main/java/com/panic/sign/ColorUtils.java
+++ app/src/main/java/com/panic/sign/ColorUtils.java
@@ -4,37 +4,49 @@ import android.content.Context;
 import android.support.v4.content.ContextCompat;
 
 public class ColorUtils {
+    // check for a way to reference the xml ids directly
+    private final static String RED = "RED";
+    private final static String ORANGE = "ORANGE";
+    private final static String YELLOW = "YELLOW";
+    private final static String GREEN = "GREEN";
+    private final static String GREEN2 = "GREEN2";
+    private final static String TEAL = "TEAL";
+    private final static String LIGHTBLUE = "LIGHTBLUE";
+    private final static String BLUE = "BLUE";
+    private final static String PURPLE = "PURPLE";
+    private final static String PINK = "PINK";
+
     public static int resolveColor(Context context, String color) {
         int id;
         switch (color) {
-            case "RED":
+            case RED:
                 id = R.color.red;
                 break;
-            case "ORANGE":
+            case ORANGE:
                 id = R.color.orange;
                 break;
-            case "YELLOW":
+            case YELLOW:
                 id = R.color.yellow;
                 break;
-            case "GREEN":
+            case GREEN:
                 id = R.color.green;
                 break;
-            case "GREEN2":
+            case GREEN2:
                 id = R.color.green2;
                 break;
-            case "TEAL":
+            case TEAL:
                 id = R.color.teal;
                 break;
-            case "LIGHTBLUE":
+            case LIGHTBLUE:
                 id = R.color.lightblue;
                 break;
-            case "BLUE":
+            case BLUE:
                 id = R.color.blue;
                 break;
-            case "PURPLE":
+            case PURPLE:
                 id = R.color.purple;
                 break;
-            case "PINK":
+            case PINK:
                 id = R.color.pink;
                 break;
             default:
