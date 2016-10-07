b69b71f2b7d87920970a5f36a1997e34bc614d27
Bastian Rinsche <bastian.rinsche@gmail.com>
2016-10-07 18:11:00 +0200

    Upgrade Leakanary to v1.5

--- app/src/main/java/de/bastianrinsche/panicsign/PanicSign.java
+++ app/src/main/java/de/bastianrinsche/panicsign/PanicSign.java
@@ -18,6 +18,12 @@ public class PanicSign extends Application {
     public void onCreate() {
         super.onCreate();
         context = this.getApplicationContext();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         LeakCanary.install(this);
 
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
