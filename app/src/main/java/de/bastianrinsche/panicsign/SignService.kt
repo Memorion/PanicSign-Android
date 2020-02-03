ca17562a904d8f1397ba0462997a878ca9805b27
Bastian Rinsche <bastian.rinsche@gmail.com>
2020-02-03 22:06:45 +0100

    Add Kotlin

---
+++ app/src/main/java/de/bastianrinsche/panicsign/SignService.kt
@@ -0,0 +1,13 @@
+package de.bastianrinsche.panicsign
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+internal interface SignService {
+    @Headers("Origin: https://sign.panic.com", "User-Agent: Panic Sign Android")
+    @GET("set/{topColor}/{bottomColor}")
+    fun setSignColors(@Path(value = "topColor", encoded = true) topColor: String?,
+                      @Path(value = "bottomColor", encoded = true) bottomColor: String?): Call<Void?>?
+}
