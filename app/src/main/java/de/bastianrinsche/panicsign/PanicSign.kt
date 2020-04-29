2054c49f3328337da44507c7b61c521fd6da4c07
Bastian Rinsche <bastian.rinsche@gmail.com>
2020-04-29 23:17:31 +0200

    Refactor retrofit creation

--- app/src/main/java/de/bastianrinsche/panicsign/PanicSign.kt
+++ app/src/main/java/de/bastianrinsche/panicsign/PanicSign.kt
@@ -1,11 +1,6 @@
 package de.bastianrinsche.panicsign
 
 import android.app.Application
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import timber.log.Timber
-import timber.log.Timber.DebugTree
 
 class PanicSign : Application() {
     override fun onCreate() {
@@ -15,23 +10,8 @@ class PanicSign : Application() {
 
     companion object {
         lateinit var colorUtils: ColorUtils
-
-        private val client = if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
-            val logging = HttpLoggingInterceptor()
-            logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
-            OkHttpClient.Builder()
-                    .addInterceptor(logging)
-                    .build()
-        } else {
-            OkHttpClient.Builder().build()
+        val signService by lazy {
+            SignService.create()
         }
-
-        private val retrofit: Retrofit = Retrofit.Builder()
-                .client(client)
-                .baseUrl("https://signserver.panic.com/")
-                .build()
-
-        var signService: SignService = retrofit.create(SignService::class.java)
     }
 }
--- app/src/main/java/de/bastianrinsche/panicsign/SignService.kt
+++ app/src/main/java/de/bastianrinsche/panicsign/SignService.kt
@@ -1,9 +1,13 @@
 package de.bastianrinsche.panicsign
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
+import timber.log.Timber
 
 interface SignService {
     @Headers("Origin: https://sign.panic.com", "User-Agent: Panic Sign Android")
@@ -12,4 +16,26 @@ interface SignService {
             @Path(value = "topColor", encoded = true) topColor: String,
             @Path(value = "bottomColor", encoded = true) bottomColor: String
     ): Response<Unit>
+
+    companion object {
+        fun create(): SignService {
+            val client = if (BuildConfig.DEBUG) {
+                Timber.plant(Timber.DebugTree())
+                val logging = HttpLoggingInterceptor()
+                logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+                OkHttpClient.Builder()
+                        .addInterceptor(logging)
+                        .build()
+            } else {
+                OkHttpClient.Builder().build()
+            }
+
+            val retrofit: Retrofit = Retrofit.Builder()
+                    .client(client)
+                    .baseUrl("https://signserver.panic.com/")
+                    .build()
+
+            return retrofit.create(SignService::class.java)
+        }
+    }
 }
